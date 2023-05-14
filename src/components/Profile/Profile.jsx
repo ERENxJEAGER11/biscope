3c9efc4bc5d2ecf5568555c2577f13100b18081f
Zaheer-fexle <newzk16117541@gmail.com>
2023-05-14 13:11:28 +0530

    Profile code to the specified

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,12 +1,47 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Box, Typography, Button } from '@mui/material';
+import { ExitToApp } from '@mui/icons-material';
 import { userSelector } from '../../features/auth';
 
 function Profile() {
   const {
     user: { username },
   } = useSelector(userSelector);
-  return <>Profile - {username}</>;
+
+  const logout = () => {
+    localStorage.clear();
+    window.location.href = '/';
+  };
+
+  const favoriteMovies = [];
+  return (
+    <Box>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h4" component="h4">
+          My Profile
+        </Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          startIcon={<ExitToApp />}
+          onClick={logout}>
+          Logout
+        </Button>
+      </Box>
+      {!favoriteMovies.lenght ? (
+        <Typography variant="h5" component="h5">
+          You have no favorite movies
+        </Typography>
+      ) : (
+        <Box>
+          <Typography variant="h5" component="h5">
+            Your favorite movies
+          </Typography>
+        </Box>
+      )}
+    </Box>
+  );
 }
 
 export default Profile;
